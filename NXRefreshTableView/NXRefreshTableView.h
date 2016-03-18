373b4d230fb6180af66b8c429c0ea3fc569f1a8d
nightx <qufeng33@hotmail.com>
2016-03-18 15:53:00 +0800

    init

---
+++ NXRefreshTableView/NXRefreshTableView.h
@@ -0,0 +1,12 @@
+//
+//  NXRefreshTableView.h
+//
+//  Created by 蒋瞿风 on 16/3/18.
+//  Copyright © 2016年 maitianer. All rights reserved.
+//
+
+#import <TPKeyboardAvoiding/TPKeyboardAvoidingTableView.h>
+
+@interface NXRefreshTableView : UITableView
+
+@end
---
+++ NXRefreshTableView/NXRefreshTableView.m
@@ -0,0 +1,41 @@
+//
+//  NXRefreshTableView.m
+//
+//  Created by 蒋瞿风 on 16/3/18.
+//  Copyright © 2016年 maitianer. All rights reserved.
+//
+
+#import "NXRefreshTableView.h"
+
+@implementation NXRefreshTableView
+
+- (instancetype)init{
+    if (self = [super init]) {
+        
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    if (self = [super initWithCoder:aDecoder]) {
+        
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame style:(UITableViewStyle)style{
+    if (self = [super initWithFrame:frame style:style]) {
+        
+    }
+    return self;
+}
+
+
+@end
